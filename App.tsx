c2cfd251fe103b460b04d31f185551461fd1274d
Abhishek <abhishek.tanwar@cloverventures.in>
2024-01-16 19:13:00 +0530

    AT |

--- App.tsx
+++ App.tsx
@@ -2,20 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import RestaurantsContainer from './components/RestaurantsContainer';
+import { Outlet, Route, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import About from './components/About';
+import Error from './components/Error';
 
 const App = () => {
+
   
   return (
     <div className='app'>
       <Header />
-      <RestaurantsContainer />
+      <Outlet />
     </div>
   )
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<App />,
+    errorElement: <Error />,
+    children:[
+      {
+        path:'/',
+        element:<RestaurantsContainer />
+      },
+      {
+        path:'/about',
+        element:<About />,
+      }
+      
+    ]
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
